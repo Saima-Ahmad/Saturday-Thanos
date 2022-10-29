14713f10d5d071ab9c235fe9e82829d2a059694a
Saima-Ahmad <saimeakhan@yahoo.com>
2022-10-29 14:17:11 -0400

    2nd commit

--- src/com/upskill/java_3/Constructor.java
+++ src/com/upskill/java_3/Constructor.java
@@ -29,6 +29,7 @@ public class Constructor extends MethodType{
 		System.out.println(obj.a);
 		System.out.println(obj.b);
 		System.out.println(obj.c);
+		System.out.println("Enter your number");
 		
 								
 	}
